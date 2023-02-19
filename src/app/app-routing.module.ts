050206346ba02b297057c4b318c9bba9166bbbff
melvinthomz <124457047+melvinthomz@users.noreply.github.com>
2023-02-19 16:38:34 +0530

    firstedit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactusComponent } from './pages/contactus/contactus/contactus.component';
+import { EquipmentsComponent } from './pages/equipments/equipments/equipments.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { SignupComponent } from './pages/signup/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",component:HomeComponent
+  },
+  {
+    path:"equipments",component:EquipmentsComponent
+  },
+  {
+    path:"signup",component:SignupComponent
+  },
+  {
+    path:"contactus",component:ContactusComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { SignupComponent } from './pages/signup/signup/signup.component';
+import { ContactusComponent } from './pages/contactus/contactus/contactus.component';
+import { NavbarComponent } from './util/navbar/navbar/navbar.component';
+import { FooterComponent } from './util/footer/footer/footer.component';
+import { EquipmentsComponent } from './pages/equipments/equipments/equipments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    ContactusComponent,
+    NavbarComponent,
+    FooterComponent,
+    EquipmentsComponent
   ],
   imports: [
     BrowserModule,
